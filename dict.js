fd78803270336797b5e80a940e12cf64272163e9
Aswin Infant Joseph <ashinfant@gmail.com>
2021-06-14 16:34:14 +0530

    Update indentation and line spacing

--- dict.js
+++ dict.js
@@ -9,88 +9,81 @@ var commands = {};
 
 
   //definitions of a word entered
-  commands.def=async function (parsedWord) {
-    const api_url = base+'word/'+parsedWord+'/definitions?api_key='+API_KEY;
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log("Definitons of "+parsedWord);
-    console.log(data);
-    
-  }
-  
-  //synonyms of a word entered
-commands.syn=async function (parsedWord) {
-  const api_url = base+'word/'+parsedWord+'/relatedWords?api_key='+API_KEY;
+commands.def = async function (parsedWord) {
+  const api_url = base + 'word/' + parsedWord + '/definitions?api_key=' + API_KEY;
   const response = await fetch(api_url);
   const data = await response.json();
-  console.log("Synonyms of "+parsedWord);
- //console.log(data[1].words[2]);
- var rl=data[0].relationshipType;
- var n = rl.localeCompare('antonym');
- if(n==0){
-   console.log(data[1]);
- }
- else{
+  console.log("Definitons of " + parsedWord);
+  console.log(data);  
+}
+  
+  //synonyms of a word entered
+commands.syn = async function (parsedWord) {
+  const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log("Synonyms of " + parsedWord);
+  var relationType = data[0].relationshipType;
+  var isAntonym = relationType.localeCompare('antonym');
+  if(isAntonym == 0) {
+    console.log(data[1]);
+  }
+  else {
    console.log(data[0]);
- }
+  }
 }
 
 //anotnyms of a word entered
 
-commands.ant=async function (parsedWord) {
-  const api_url = base+'word/'+parsedWord+'/relatedWords?api_key='+API_KEY;
+commands.ant = async function (parsedWord) {
+  const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
   const response = await fetch(api_url);
   const data = await response.json();
-
- //console.log(word.words[2]);
- rl=data[0].relationshipType;
- var n = rl.localeCompare('antonym');
- if(n==0){
-  console.log("Antonyms of "+parsedWord);
-   console.log(data[0]);
- }
- else{
-   console.log("No antonyms present");
- }
+  var relationType = data[0].relationshipType;
+  var isAntonym = relationType.localeCompare('antonym');
+  if(isAntonym == 0) {
+    console.log("Antonyms of " + parsedWord);
+    console.log(data[0]);
+  }
+  else {
+    console.log("No antonyms present");
+  }
 }
 
   //examples of a word entered
-  commands.ex=async function (parsedWord) {
-    const api_url = base+'word/'+parsedWord+'/examples?api_key='+API_KEY;
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log("Examples of "+parsedWord);
-   console.log(data);
-   
-  }
+commands.ex = async function (parsedWord) {
+  const api_url = base + 'word/' + parsedWord + '/examples?api_key=' + API_KEY;
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log("Examples of " + parsedWord);
+  console.log(data); 
+}
 
   //entire dictionary of a word entered
-  commands.dict = function (parsedWord)  {
-    commands.def(parsedWord);
-    commands.syn(parsedWord);
-    commands.ex(parsedWord);
-   
-  }
-  commands.play = function ()  {
-    console.log("game starts");
-    lib.gameplay();
-  }
+commands.dict = function (parsedWord)  {
+  commands.def(parsedWord);
+  commands.syn(parsedWord);
+  commands.ex(parsedWord); 
+}
+commands.play = function ()  {
+  console.log("game starts");
+  lib.gameplay();
+}
 
  
   //random word of the day's dictionary 
-  async function day() {
-    const api_url = base+'words/randomWord?api_key='+API_KEY;
-    const response = await fetch(api_url);
-    const data = await response.json();
-    
-   var mainWord=data.word;
-   console.log("Word of the day is:"+mainWord+"\n");
-   commands.dict(mainWord);
-    }
+async function day() {
+  const api_url = base + 'words/randomWord?api_key=' + API_KEY;
+  const response = await fetch(api_url);
+  const data = await response.json();  
+  var mainWord = data.word;
+  console.log("Word of the day is:" + mainWord + "\n");
+  commands.dict(mainWord);
+}
 
-  if (typeof commands[type] === 'function') {
-    commands[type](argument)
-  }
-  if (typeof commands[type] === "undefined") {
-    day();
-  }
+if (typeof commands[type] === 'function') {
+  commands[type](argument)
+}
+if (typeof commands[type] === "undefined") {
+  day();
+}

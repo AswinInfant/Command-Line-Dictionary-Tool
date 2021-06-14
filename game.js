be342f4f1bfcac83e1f5beb0c343511599a333f4
Aswin Infant Joseph <ashinfant@gmail.com>
2021-06-14 16:35:02 +0530

    Updated indentation and line spacing

--- game.js
+++ game.js
@@ -9,208 +9,200 @@ var defhint,synhint,anthint,hintExhaust;
 var score=0;
 var synonyms=[];
 var i;
-  commands.def=async function (parsedWord,j) {
-    const api_url = base+'word/'+parsedWord+'/definitions?api_key='+API_KEY;
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log("Definition");
+  
+commands.def = async function (parsedWord, j) {
+  const api_url = base + 'word/' + parsedWord + '/definitions?api_key=' + API_KEY;
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log("Definition");
     
-    if (typeof data[j] === "undefined") {
-      console.log("No more definitions available,choose other hint");
-      hintExhaust++;
-      score+=3;
-      menu(2,parsedWord);
-    }
-    else{
-      console.log(data[j]);
-   guess = prompt('Guess the word');
-   check(guess,parsedWord);
-    }
-    
+  if (typeof data[j] === "undefined") {
+    console.log("No more definitions available,choose other hint");
+    hintExhaust++;
+    score+= 3;
+    menu(2, parsedWord);
   }
+  else { 
+    console.log(data[j]);
+    guess = prompt('Guess the word');
+    check(guess, parsedWord);
+  } 
+}
 
-    commands.syn=async function (parsedWord,j) {
-    const api_url = base+'word/'+parsedWord+'/relatedWords?api_key='+API_KEY;
-    const response = await fetch(api_url);
-    const data = await response.json();
-   var rl=data[0].relationshipType;
-   var n = rl.localeCompare('antonym');
+commands.syn = async function (parsedWord, j) {
+  const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
+  const response = await fetch(api_url);
+  const data = await response.json();
+  var relationType = data[0].relationshipType;
+  var isAntonym = relationType.localeCompare('antonym');
    
-   if(n==0){
-      for(i = 0; typeof data[1].words[i] != "undefined"; i++){
-        synonyms[i]=data[1].words[i];
-      }
+  if(isAntonym == 0) {
+    for (i = 0; typeof data[1].words[i] != "undefined"; i++) {
+      synonyms[i] = data[1].words[i];
+    }
     if (typeof data[1].words[j] === "undefined") {
       console.log("No more synonyms available,choose other hint");
       hintExhaust++;
-      score+=3;
-      menu(2,parsedWord);
+      score+= 3;
+      menu(2, parsedWord);
     }
-    else{
-      console.log("Synonym for the given word: "+synonyms[j]);
+    else {
+      console.log("Synonym for the given word: " + synonyms[j]);
       synRemove(synonyms[j]);
-   guess = prompt('Guess the word');
-   check(guess,parsedWord);
+      guess = prompt('Guess the word');
+      check(guess, parsedWord);
+    } 
+  }
+  else { 
+    for (i = 0; typeof data[0].words[i] != "undefined"; i++) { 
+      synonyms[i] = data[0].words[i];
     }
-     
-   }
-   else{
-    for(i = 0; typeof data[0].words[i] != "undefined"; i++){
-      synonyms[i]=data[0].words[i];
-    }
-     if (typeof data[0].words[j] === "undefined") {
+    if (typeof data[0].words[j] === "undefined") {
       console.log("No more synonyms available,choose other hint");
       hintExhaust++;
-      score+=3;
-      menu(2,parsedWord);
+      score+= 3;
+      menu(2, parsedWord);
     }
-    else{
-      console.log("Synonym for the given word: "+synonyms[j]);
+    else { 
+      console.log("Synonym for the given word: " + synonyms[j]);
       synRemove(synonyms[j]);
-   guess = prompt('Guess the word');
-   check(guess,parsedWord);
+      guess = prompt('Guess the word');
+      check(guess, parsedWord);
     }
-   }
   }
+}
 
 
-  commands.ant=async function (parsedWord,j) {
-    const api_url = base+'word/'+parsedWord+'/relatedWords?api_key='+API_KEY;
-    const response = await fetch(api_url);
-    const data = await response.json();
-    console.log("Antonym");
-   rl=data[0].relationshipType;
-   var n = rl.localeCompare('antonym');
-   if(n==0){
+commands.ant = async function (parsedWord,j) {
+  const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
+  const response = await fetch(api_url);
+  const data = await response.json();
+  console.log("Antonym");
+  var relationType = data[0].relationshipType;
+  var isAntonym = relationType.localeCompare('antonym');
+  if(isAntonym == 0) {
     if (typeof data[0].words[j] === "undefined") {
       console.log("No more antonyms available,choose other hint");
       hintExhaust++;
       score+=3;
-      menu(2,parsedWord);
+      menu(2, parsedWord);
     }
-    else{
-      console.log("Antonym for the given word: "+data[0].words[j]);
-   guess = prompt('Guess the word');
-   check(guess,parsedWord);
-    }
-     
-   }
-   else{
-     console.log("No antonyms present, try another hint");
-     score+=3;
-     hintExhaust++;
-     menu(2,parsedWord);
-   }
-   
-  }
-
-  function synRemove(guessword2){
-    for(i = 0; i < synonyms.length; i++){
-      if(synonyms[i].localeCompare(guessword2) == 0){
-        synonyms.splice(i,1);
-      }
-    }
-  } 
-
-  function synCompare(guessword1){
-    var flag=synonyms.includes(guessword1);
-    if(flag==1){
-      synRemove(guessword1);
-      return flag; 
-    }
-    else{
-      return flag;
+    else {
+      console.log("Antonym for the given word: " + data[0].words[j]);
+      guess = prompt('Guess the word');
+      check(guess, parsedWord);
     }
   }
+  else {
+    console.log("No antonyms present, try another hint");
+    score+= 3;
+    hintExhaust++;
+    menu(2 , parsedWord);
+  }
+}
 
- function check(guessword,apiword){
-   if(guessword.localeCompare(apiword)==0){
-     console.log("correct");
-     score+=10;
-     console.log("Your score= "+score);
-     gameplay();
-   }
-   else if(synCompare(guessword) == 1){
-     console.log("Syn correct");
-     gameplay();
-   }
-   else{
-     console.log("wrong");
-     score-=2;
-     console.log("Your score= "+score);
+function synRemove(guesswordRemove) {
+  for (i = 0; i < synonyms.length; i++) {
+    if(synonyms[i].localeCompare(guesswordRemove) == 0) {
+      synonyms.splice(i,1);
+    }
+  }
+} 
+
+function synCompare(guesswordCompare) {
+  var isPresent = synonyms.includes(guesswordCompare);
+  if(isPresent == 1) {
+    synRemove(guesswordCompare);
+    return isPresent; 
+  }
+  else {
+    return isPresent;
+  }
+}
+
+function check(guessword,apiword) {
+  if(guessword.localeCompare(apiword) == 0) {
+    console.log("correct");
+    score+= 10;
+    console.log("Your score= "+score);
+    gameplay();
+  }
+  else if(synCompare(guessword) == 1) {
+    console.log("Syn correct");
+    gameplay();
+  }
+  else {
+    console.log("wrong");
+    score-= 2;
+    console.log("Your score= " + score);
     var opt = prompt('Press a number for hint---1.Try again---2.Hint---3.SKip');
     menu(opt,apiword);
-   }
- }
-
- function jumble(a){
-   a=a.split("");
-   for(var b=a.length-1;0<b;b--){
-     var c=Math.floor(Math.random()*(b+1));
-     d=a[b];
-     a[b]=a[c];
-     a[c]=d
-    }
-    return a.join("")
   }
+}
 
+function jumble(a) {
+  a=a.split("");
+  for (var b = a.length-1; 0 < b; b--) {
+    var c=Math.floor(Math.random()*(b+1));
+    d=a[b];
+    a[b]=a[c];
+    a[c]=d
+  }
+  return a.join("")
+}
 
-
-function menu(option,apiword1){
-  if(option ==1){
+function menu(option, apiword1) {
+  if(option == 1) {
     var guess = prompt('Guess the word');
     console.log(guess);
-    check(guess,apiword1);
+    check(guess, apiword1);
   }
-  else if(option==2)
-  {
-    if(hintExhaust==3){
+  else if(option == 2) {
+    if(hintExhaust == 3) {
       console.log("jumbled word");
       console.log(jumble(apiword1));
       var guess = prompt('Guess the word');
-    console.log(guess);
-    check(guess,apiword1);
+      console.log(guess);
+      check(guess,apiword1);
     }
-    else{
-    var hintopt = prompt('What hint do you want--1.Definition--2.Synonyms--3.Antonyms');
-    if(hintopt==1){
-    score-=3;
-    console.log("Your score= "+score);
-    commands.def(apiword1,defhint);
-    defhint++;
-    }
-    else if(hintopt==2){
-      score-=3;
-      console.log("Your score= "+score);
-      commands.syn(apiword1,synhint);
-      synhint++;
+    else {
+      var hintopt = prompt('What hint do you want--1.Definition--2.Synonyms--3.Antonyms');
+      if(hintopt==1) {
+        score-=3;
+        console.log("Your score= "+score);
+        commands.def(apiword1,defhint);
+        defhint++;
       }
-      else if(hintopt==3){
+      else if(hintopt==2) {
+        score-=3;
+        console.log("Your score= "+score);
+        commands.syn(apiword1,synhint);
+        synhint++;
+      }
+      else if(hintopt==3) {
         score-=3;
         console.log("Your score= "+score);
         commands.ant(apiword1,anthint);
         anthint++;
-        }
-     }
+      }
     }
-
-
-  else if(option==3){
+  }
+  else if(option == 3) {
     console.log(apiword1);
+    gameplay();
   }
 }
 
-  async function gameplay()  {
-    defhint=1;synhint=0;anthint=0,hintExhaust=0;
-    const api_url = base+'words/randomWord?api_key='+API_KEY;
-    const response = await fetch(api_url);
-    const data = await response.json();
-    var mainWord=data.word;
-    var i=0;
-    console.log(mainWord);
-    commands.def(mainWord,0);
-
-  }
+async function gameplay()  {
+  defhint = 1; synhint = 0; anthint = 0; hintExhaust = 0;
+  const api_url = base + 'words/randomWord?api_key=' + API_KEY;
+  const response = await fetch(api_url);
+  const data = await response.json();
+  var mainWord = data.word;
+  var i = 0;
+  console.log(mainWord);
+  commands.def(mainWord, 0);
+}
  
 
 

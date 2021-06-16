d532db2ed0f92ed4deff07d0330f50d9e9d4f9f8
Aswin Infant Joseph <ashinfant@gmail.com>
2021-06-16 17:08:34 +0530

    Update dict.js

--- dict.js
+++ dict.js
@@ -6,64 +6,81 @@ var API_KEY= 'fb8007781a73a8884e3821dc8f330cf2949b422d2a4be2bac9f1d5def50213d48f
 const type = process.argv[2] ;
 const argument = process.argv[3];
 var commands = {};
+var data;
 
+async function apiCall(parsedWord, commandType) {
+  if(commandType.localeCompare('definition') == 0) {
+    const api_url = base + 'word/' + parsedWord + '/definitions?api_key=' + API_KEY;
+    const response = await fetch(api_url);
+    data = await response.json();
+    console.log(data);
+  }
+  else if(commandType.localeCompare('synonyms') == 0) {
+    const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log("Synonyms of " + parsedWord);
+    var relationType = data[0].relationshipType;
+    var isAntonym = relationType.localeCompare('antonym');
+    if(isAntonym == 0) {
+      console.log(data[1]);
+    }
+    else {
+    console.log(data[0]);
+    }
+  }
+  else if(commandType.localeCompare('antonyms') == 0) {
+    const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
+    const response = await fetch(api_url);
+    const data = await response.json();
+    var relationType = data[0].relationshipType;
+    var isAntonym = relationType.localeCompare('antonym');
+    if(isAntonym == 0) {
+      console.log("Antonyms of " + parsedWord);
+      console.log(data[0]);
+    }
+    else {
+      console.log("No antonyms present");
+    }
+  }
+  else if(commandType.localeCompare('examples') == 0) {
+    const api_url = base + 'word/' + parsedWord + '/examples?api_key=' + API_KEY;
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log("Examples of " + parsedWord);
+    console.log(data);
+  }
+}
 
-  //definitions of a word entered
+//definitions of a word entered
 commands.def = async function (parsedWord) {
-  const api_url = base + 'word/' + parsedWord + '/definitions?api_key=' + API_KEY;
-  const response = await fetch(api_url);
-  const data = await response.json();
   console.log("Definitons of " + parsedWord);
-  console.log(data);  
+  apiCall(parsedWord, 'definition'); 
 }
   
-  //synonyms of a word entered
+//synonyms of a word entered
 commands.syn = async function (parsedWord) {
-  const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
-  const response = await fetch(api_url);
-  const data = await response.json();
   console.log("Synonyms of " + parsedWord);
-  var relationType = data[0].relationshipType;
-  var isAntonym = relationType.localeCompare('antonym');
-  if(isAntonym == 0) {
-    console.log(data[1]);
-  }
-  else {
-   console.log(data[0]);
-  }
+  apiCall(parsedWord, 'synonyms');
 }
 
 //anotnyms of a word entered
-
 commands.ant = async function (parsedWord) {
-  const api_url = base + 'word/' + parsedWord + '/relatedWords?api_key=' + API_KEY;
-  const response = await fetch(api_url);
-  const data = await response.json();
-  var relationType = data[0].relationshipType;
-  var isAntonym = relationType.localeCompare('antonym');
-  if(isAntonym == 0) {
-    console.log("Antonyms of " + parsedWord);
-    console.log(data[0]);
-  }
-  else {
-    console.log("No antonyms present");
-  }
+  apiCall(parsedWord, 'antonyms');
 }
 
   //examples of a word entered
 commands.ex = async function (parsedWord) {
-  const api_url = base + 'word/' + parsedWord + '/examples?api_key=' + API_KEY;
-  const response = await fetch(api_url);
-  const data = await response.json();
-  console.log("Examples of " + parsedWord);
-  console.log(data); 
+  apiCall(parsedWord, 'examples'); 
 }
 
   //entire dictionary of a word entered
 commands.dict = function (parsedWord)  {
-  commands.def(parsedWord);
-  commands.syn(parsedWord);
-  commands.ex(parsedWord); 
+  apiCall(parsedWord, 'definition');
+  apiCall(parsedWord, 'synonyms');
+  apiCall(parsedWord, 'antonyms');
+  apiCall(parsedWord, 'examples'); 
+  
 }
 commands.play = function ()  {
   console.log("game starts");
